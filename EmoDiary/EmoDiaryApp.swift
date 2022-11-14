bc34ea71e7c7009c988ba1887b9a7c4c079def12
deskside's iMac <deskside.zhuoyan@outlook.com>
2022-11-14 14:00:31 +0800

    Initial Commit test.

---
+++ EmoDiary/EmoDiaryApp.swift
@@ -0,0 +1,20 @@
+//
+//  EmoDiaryApp.swift
+//  EmoDiary
+//
+//  Created by Joey Xie on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct EmoDiaryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
